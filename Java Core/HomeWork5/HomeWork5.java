33049c7b94c270cd5313b5d949c31cfca273023d
ButakovaAO <92058336+ButakovaAO@users.noreply.github.com>
2021-12-16 19:53:05 +0300

    Update HomeWork5.java

--- Java Core/HomeWork5/HomeWork5.java
+++ Java Core/HomeWork5/HomeWork5.java
@@ -13,36 +13,36 @@ import java.io.IOException;
 
 public class HomeWork5 {
     public static void main(String[] args) {
-    try (PrintWriter writer = new PrintWriter(new File("HomeWork5.csv"))) {
+        try (PrintWriter writer = new PrintWriter(new File("HomeWork5.csv"))) {
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("Value 1");
-        sb.append(';');
-        sb.append("Value 2");
-        sb.append(';');
-        sb.append("Value 3");
-        sb.append('\n');
+            StringBuilder sb = new StringBuilder();
+            sb.append("Value 1");
+            sb.append(';');
+            sb.append("Value 2");
+            sb.append(';');
+            sb.append("Value 3");
+            sb.append('\n');
 
-        sb.append("100");
-        sb.append(';');
-        sb.append("200");
-        sb.append(';');
-        sb.append("123");
-        sb.append('\n');
+            sb.append("100");
+            sb.append(';');
+            sb.append("200");
+            sb.append(';');
+            sb.append("123");
+            sb.append('\n');
 
-        sb.append("300");
-        sb.append(';');
-        sb.append("400");
-        sb.append(';');
-        sb.append("500");
-        sb.append('\n');
+            sb.append("300");
+            sb.append(';');
+            sb.append("400");
+            sb.append(';');
+            sb.append("500");
+            sb.append('\n');
 
-        writer.write(sb.toString());
+            writer.write(sb.toString());
 
-        System.out.println("done!");
+            System.out.println("done!");
 
-    } catch (FileNotFoundException e) {
-        System.out.println(e.getMessage());
-    }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
